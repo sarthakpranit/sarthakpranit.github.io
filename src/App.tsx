263c5ad5dd40cf3cb8f592ea7060542ba5944591
Sarthak <sarthakpranit@gmail.com>
2025-03-24 18:58:32 +0100

    feat: add feature flag for password protection

--- src/App.tsx
+++ src/App.tsx
@@ -8,31 +8,40 @@ import { ThemeProvider } from "./hooks/use-theme";
 import PasswordProtection from "./components/PasswordProtection";
 import { MainLayout } from "./layouts/MainLayout";
 import { routes } from "./routes";
+import { FEATURES } from "./config/features";
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <ThemeProvider>
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
-        <BrowserRouter>
-          <PasswordProtection>
-            <MainLayout>
-              <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                  {routes.map((route, index) => (
-                    <Route key={index} path={route.path} element={route.element} />
-                  ))}
-                </Routes>
-              </Suspense>
-            </MainLayout>
-          </PasswordProtection>
-        </BrowserRouter>
-      </TooltipProvider>
-    </ThemeProvider>
-  </QueryClientProvider>
-);
+const App = () => {
+  const content = (
+    <MainLayout>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          {routes.map((route, index) => (
+            <Route key={index} path={route.path} element={route.element} />
+          ))}
+        </Routes>
+      </Suspense>
+    </MainLayout>
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <TooltipProvider>
+          <Toaster />
+          <Sonner />
+          <BrowserRouter>
+            {FEATURES.PASSWORD_PROTECTION ? (
+              <PasswordProtection>{content}</PasswordProtection>
+            ) : (
+              content
+            )}
+          </BrowserRouter>
+        </TooltipProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
+};
 
 export default App;
---
+++ src/config/features.ts
@@ -0,0 +1,3 @@
+export const FEATURES = {
+  PASSWORD_PROTECTION: false, // Set to true to enable password protection
+} as const; 
